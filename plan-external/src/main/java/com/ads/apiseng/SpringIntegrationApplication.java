99dcd4fa70ed4f8972d516135f244e534d171a86
Shahzad R. <shahzad.riasat@gmail.com>
2025-09-10 15:01:31 -0500

    ok

--- plan-external/src/main/java/com/ads/apiseng/SpringIntegrationApplication.java
+++ plan-external/src/main/java/com/ads/apiseng/SpringIntegrationApplication.java
@@ -7,11 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringIntegrationApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringIntegrationApplication.class, args);
-
-        System.out.println("=================================================");
-        System.out.println("Spring Integration External XML Demo started.");
-        System.out.println("Looking for /tmp/integration.xml");
-        System.out.println("If missing, no flows will be active.");
-        System.out.println("=================================================");
+        System.out.println("===============================================");
+        System.out.println("Spring Integration Demo App started.");
+        System.out.println("Looking for external config in /tmp/integration.xml");
+        System.out.println("If not present, no flows will be active.");
+        System.out.println("===============================================");
     }
 }
